3a638154c351a9a629f2cc9d07d10fa1d0d512be
Suresh-R-S <rssuresh2@gmail.com>
2018-11-29 17:51:53 +0530

    Integrated chart interaction, style changes, local storage actions

--- src/components/chartData/index.js
+++ src/components/chartData/index.js
@@ -1,30 +1,60 @@
 import React, {Component} from 'react';
-import {cloneDeep, isEqual} from 'lodash';
-import {Grid, Modal, TextField, Button} from '@material-ui/core';
+import {cloneDeep} from 'lodash';
+import { CompactPicker } from 'react-color'
+import {
+    Grid,
+    Modal, 
+    TextField, 
+    Button, 
+    Icon, 
+    FormControl, 
+    FormControlLabel, 
+    FormGroup, 
+    FormLabel, 
+    Checkbox,
+    Tooltip,
+    IconButton,
+} from '@material-ui/core';
 
 import './style.css';
 
 export default class ChartData extends Component {
 
     state = {
-        data : cloneDeep(this.props.modalData)
+        data : cloneDeep(this.props.modalData),
+        layoutData: []
     }
 
-    // shouldComponentUpdate = (newProps, newState) => {
-    //     return (
-    //         this.props.open !== newProps.open || 
-    //         !isEqual(this.state.data, newState.data)
-    //     );
-    // } 
+    componentDidMount () {
+        this.setState({
+            layoutData: this.createLayoutData(this.props.layout)
+        });
+    }
 
-    // componentWillReceiveProps = (newProps) => {
-    //     if( !isEqual(this.props.modalData, newProps.modalData) ) {
-    //         console.log('newProps.modalData', newProps);
-    //         this.setState({
-    //             data : cloneDeep(newProps.modalData)
-    //         })
-    //     }
-    // }
+    componentWillReceiveProps = (newProps) => {
+        if( !this.props.open && newProps.open ) {
+            this.setState({
+                data : cloneDeep(newProps.modalData),
+                layoutData: this.createLayoutData(newProps.layout, newProps.chartIndex)
+            })
+        }
+
+        if( this.props.chartIndex !== newProps.chartIndex ) {
+            this.setState({
+                layoutData: this.createLayoutData(newProps.layout)
+            })
+        }
+    }
+
+    createLayoutData = (data) => {
+        return data.map( item => {
+            return {
+                name: item.name,
+                index: parseInt(item.i),
+                checked: false
+            }
+        } );
+    }
 
     handleDataChange = (type, index, textVal) => {
         const data = cloneDeep(this.state.data);
@@ -43,12 +73,82 @@ export default class ChartData extends Component {
         })
     }
 
+    handleColorChange = (color, index) => {
+        const data = cloneDeep(this.state.data);
+        const backgroundColor = [...data.datasets[0].backgroundColor];
+        const hoverBackgroundColor = [...data.datasets[0].hoverBackgroundColor];
+        backgroundColor[index] = color;
+        hoverBackgroundColor[index] = color;
+        data.datasets[0].backgroundColor = [...backgroundColor];
+        data.datasets[0].hoverBackgroundColor = [...hoverBackgroundColor];
+        this.setState({data});
+    }
+
+    addNewData = () => {
+        const data = cloneDeep(this.state.data);
+        data.labels = [ ...this.state.data.labels, 'Label'];
+        data.datasets[0].data = [ ...this.state.data.datasets[0].data, 100];
+        data.datasets[0].backgroundColor = [ ...this.state.data.datasets[0].backgroundColor, '#FFFFF'];
+        data.datasets[0].hoverBackgroundColor = [ ...this.state.data.datasets[0].hoverBackgroundColor, '#FFFFF'];
+        this.setState({data});
+    }
+
+    removeData = (index) => {
+        const data = cloneDeep(this.state.data);
+        data.labels.splice(index,1);
+        data.datasets[0].data.splice(index,1);
+        data.datasets[0].backgroundColor.splice(index,1);
+        data.datasets[0].hoverBackgroundColor.splice(index,1);
+        this.setState({data});
+    }
+
+    handleCheckBoxChange = (e, index) => {
+        const layoutData = cloneDeep(this.state.layoutData);
+        layoutData[index].checked = !layoutData[index].checked;
+        this.setState({layoutData});
+    }
+
+    handleOnSave = () => {
+        const indexArr = this.state.layoutData.reduce( (newArr, item) => {
+            if(item.checked) {
+                newArr.push(item.index);
+            }
+            return newArr;
+        }, [] );
+        indexArr.push(this.props.chartIndex);
+        this.props.saveData(indexArr, this.state.data);
+    }
+
+    renderCheckBoxGroup = () => {
+        return (
+            <Grid item xs={12}>
+                <FormControl component="fieldset" className="checkbox-container">
+                    <FormLabel component="legend">Replace chart data</FormLabel>
+                    <FormGroup>
+                        {
+                            this.state.layoutData.map( (item, index) => {
+                                return (
+                                    <FormControlLabel
+                                        key={item.name}
+                                        control={
+                                            <Checkbox disabled={index === this.props.chartIndex} checked={item.checked} onChange={(e) => this.handleCheckBoxChange(e,index)} />
+                                        }
+                                        label={item.name}
+                                    />
+                                )
+                            })
+                        }
+                    </FormGroup>
+                </FormControl>
+            </Grid>
+        )
+    };
+
     renderTextFields = () => {
-        if( !this.state.data.labels ) return null;
         return(
             this.state.data.labels.map( (i, index) => (
-                <Grid container item xs={12} spacing={16}>
-                    <Grid item xs={4}>
+                <Grid container item xs={12} spacing={16} key={i}>
+                    <Grid item xs={2}>
                         <TextField
                             id={`${index}Label`}
                             label="Label"
@@ -58,7 +158,7 @@ export default class ChartData extends Component {
                             variant="outlined"
                         />
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={2}>
                         <TextField
                             id={`${index}Value`}
                             type="number"
@@ -68,6 +168,32 @@ export default class ChartData extends Component {
                             margin="normal"
                             variant="outlined"
                         />
+                    </Grid>
+                    <Grid item xs={4} className="color-picker-container">
+                        <CompactPicker
+                            color={ this.state.data.datasets[0].backgroundColor[index] }
+                            onChangeComplete={ ({hex}) => this.handleColorChange(hex, index) }
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <div className="action-button-container">
+                            {
+                                index > 1 &&
+                                <Tooltip title="Remove chart data">
+                                    <Button variant="fab" mini color="secondary" aria-label="Remove" onClick={() => this.removeData(index)}>
+                                        <Icon>delete</Icon>
+                                    </Button>
+                                </Tooltip>
+                            }
+                            {
+                                index === this.state.data.labels.length - 1 &&
+                                <Tooltip title="Add new chart data">
+                                    <Button variant="fab" mini color="primary" aria-label="Add" onClick={this.addNewData}>
+                                        <Icon>add</Icon>
+                                    </Button>
+                                </Tooltip>
+                            }
+                        </div>
                     </Grid>
                 </Grid>
             ) )
@@ -79,13 +205,25 @@ export default class ChartData extends Component {
             <Modal
                 open={this.props.open}
                 onClose={this.props.onClose}
+                className="modal"
             >
                 <div className="container">
                     <Grid container>
+                        <Grid item xs={12} className="close-modal-container">
+                        <Tooltip title="Close">
+                            <IconButton onClick={() => this.props.onClose()}>
+                                <Icon color='action'>close</Icon>
+                            </IconButton>
+                        </Tooltip>
+                        </Grid>
                         {this.renderTextFields()}
-                        <Grid container item xs={12}>
-                            <Button variant="contained" color="primary" onClick={() => this.props.saveData(this.state.data,this.props.chartIndex)}>
+                        {this.renderCheckBoxGroup()}
+                        <Grid container item xs={12} className="button-container">
+                            <Button variant="contained" color="primary" onClick={() => this.handleOnSave()}>
                                 Save
+                            </Button>
+                            <Button variant="contained" color="secondary" onClick={() => this.props.removeData()}>
+                                Remove
                             </Button>
                         </Grid>
                     </Grid>
--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Grid} from '@material-ui/core';
+import {Grid, IconButton, Icon, Tooltip} from '@material-ui/core';
 import RGL, { WidthProvider } from 'react-grid-layout';
 import {cloneDeep} from 'lodash';
 
@@ -12,38 +12,51 @@ const ReactGridLayout = WidthProvider(RGL);
 const defaultChartData = {
     labels: ['One', 'Two'],
     datasets: [{
-        data : [ 50, 50 ]
+        data : [ 50, 50 ],
+        backgroundColor: [ '#FF6384', '#36A2EB' ],
+        hoverBackgroundColor: [ '#FF6384', '#36A2EB' ]
     }]
 };
 
 export default class Dashboard extends Component {
 
-    static defaultProps = {
-        className: "layout",
-        rowHeight: 30,
-        cols: 6,
-    };
-
     state = {
-        layout: [],
+        layout: this.initialLayout(),
         open: false,
         modalData: cloneDeep(defaultChartData),
         openedChartIndex: 0,
+        clickedChart: 'pie'
     }
 
-    toggleModal = (itemData, index) => {
+    initialLayout () {
+        const layoutData = JSON.parse(localStorage.getItem('layout'));
+        return layoutData ? cloneDeep(layoutData) : [];
+    }
+
+    toggleModal = (itemData, index, type) => {
         const { open } = this.state;
         this.setState({
             open: !open,
             modalData: itemData ? cloneDeep(itemData) : cloneDeep(defaultChartData),
-            openedChartIndex: !open ? index : 0
+            openedChartIndex: !open ? index : 0,
+            clickedChart: itemData ? type : 'pie'
         });
     }
     
     generateDOM = () => {
         return this.state.layout.map( (item, index) => (
-            <div key={index} onClick={() => this.toggleModal(item.data, index)}>
-                { item.type === 'pie' ? <PieChart data={item.data}/> : <BarChart data={item.data}/>}
+            <div key={item.i} className="chart-container">
+                { item.type === 'pie' ? <PieChart data={cloneDeep(item.data)}/> : <BarChart data={cloneDeep(item.data)}/>}
+                <div className="chart-name-container">
+                    <span className="chart-name">{item.name}</span>
+                </div>
+                <div className="edit-button-container" >
+                <Tooltip title="Edit chart">
+                    <IconButton onClick={() => this.toggleModal(item.data, index, item.type)}>
+                        <Icon color='primary'>edit</Icon>
+                    </IconButton>
+                </Tooltip>
+                </div>
             </div>
         ) );
     }
@@ -60,6 +73,7 @@ export default class Dashboard extends Component {
             h: 5,
             i: i.toString(),
             type,
+            name:`${type}_chart_${i}`,
             data: cloneDeep(defaultChartData)
         });
         this.setState({
@@ -67,34 +81,83 @@ export default class Dashboard extends Component {
         });
     }
     
-    onLayoutChange = (layout) => {
-        // this.props.onLayoutChange(layout);
+    onLayoutChange = (newLayout) => {
+        const layout = cloneDeep(this.state.layout);
+        newLayout.forEach( item => {
+            layout.x = item.x;
+            layout.y = item.y;
+        } )
+        this.setState({layout});
     }
 
     widgetSelected = (type) => {
         this.generateLayout(type);
     }
 
-    changeChartData = (data,index) => {
+    changeChartData = (indexArr,data) => {
         const layout = cloneDeep(this.state.layout);
-        layout[index].data = cloneDeep(data);
-        this.setState({layout, open: false, modalData: cloneDeep(defaultChartData)});
+        indexArr.forEach((item) => {
+            layout[item].data =  cloneDeep(data);
+        })
+        this.setState({layout, open: false, modalData: cloneDeep(defaultChartData), openedChartIndex:0});
+    }
+
+    removeChartData = () => {
+        const layout = cloneDeep(this.state.layout);
+        layout.splice(this.state.openedChartIndex, 1);
+        this.setState({layout, open: false, modalData: cloneDeep(defaultChartData), openedChartIndex:0});
+    }
+
+    saveDashboard = () => {
+        localStorage.setItem('layout',JSON.stringify(this.state.layout));
+    }
+
+    clearDashboard = () => {
+        localStorage.removeItem('layout');
+        this.setState({
+            layout: [],
+            open: false,
+            modalData: cloneDeep(defaultChartData),
+            openedChartIndex: 0,
+            clickedChart: 'pie'
+        })
     }
 
     render() {
         return (
             <Grid container>
                 <Grid container item spacing={24}>
-                    <Grid item xs={1} className="widgets-container">
-                        <h3 className="title">Widgets</h3>
-                        <p className="widget-options" onClick={() => this.widgetSelected('pie')}>Pie chart</p>
-                        <p className="widget-options" onClick={() => this.widgetSelected('bar')}>Bar chart</p>
+                    <Grid item xs={12} className="widgets-container">
+                        <div className="widgets-options">
+                        <Tooltip title="Pie Chart">
+                            <IconButton onClick={() => this.widgetSelected('pie')}>
+                                <Icon color='primary'>pie_chart</Icon>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Bar Chart">
+                            <IconButton onClick={() => this.widgetSelected('bar')}>
+                                <Icon color='primary'>bar_chart</Icon>
+                            </IconButton>
+                        </Tooltip>
+                        </div>
+                        <Tooltip title="Save to local storage">
+                            <IconButton disabled={!this.state.layout.length} onClick={this.saveDashboard}>
+                                <Icon color={ this.state.layout.length ? 'primary' : 'disabled'}>save</Icon>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Clear dashboard">
+                            <IconButton disabled={!this.state.layout.length} onClick={this.clearDashboard}>
+                                <Icon color={ this.state.layout.length ? 'secondary' : 'disabled'}>delete</Icon>
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
-                    <Grid item xs={11}>
+                    <Grid item xs={12}>
                     <ReactGridLayout
                         layout={this.state.layout}
                         onLayoutChange={this.onLayoutChange}
-                        {...this.props}
+                        className="layout"
+                        rowHeight={30}
+                        cols={6}
                     >
                         {this.generateDOM()}
                     </ReactGridLayout>
@@ -104,9 +167,12 @@ export default class Dashboard extends Component {
                     <ChartData
                         open={this.state.open}
                         modalData={this.state.modalData}
-                        chartIndex={this.state.openedChartIndex}
                         onClose={() => this.toggleModal(0)}
                         saveData={this.changeChartData}
+                        removeData={this.removeChartData}
+                        chart={this.state.clickedChart}
+                        layout={this.state.layout}
+                        chartIndex={this.state.openedChartIndex}
                     />
                 </Grid>
             </Grid>
